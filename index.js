7514165ac929eacf26d98a8442506e844eba850d
asifalam515 <asibulalam515@gmail.com>
2024-11-24 10:05:01 +0600

    api created

--- index.js
+++ index.js
@@ -2,15 +2,77 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 // use middleware
 app.use(cors());
 app.use(express.json());
 
+// MONGODB part:
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri =
+//   "mongodb+srv://<db_username>:<db_password>@cluster0.6tngyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6tngyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db("bistro_db");
+    const menuCollection = database.collection("menu");
+    const reviewsCollection = database.collection("reviews");
+    const cartCollection = database.collection("carts");
+
+    // get all the menu
+    app.get("/menu", async (req, res) => {
+      const results = await menuCollection.find().toArray();
+      res.send(results);
+    });
+
+    // get all the reviews data
+    app.get("/reviews", async (req, res) => {
+      const results = await reviewsCollection.find().toArray();
+      res.send(results);
+    });
+
+    // carts collection:
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+
+      const cursor = await cartCollection.find(query).toArray();
+      res.send(cursor);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Bistro is running");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+// naming convention
