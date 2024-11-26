ae00734db3535f17eb777790f6fa268cfd3ea28d
asifalam515 <asibulalam515@gmail.com>
2024-11-26 15:44:38 +0600

    interceptor added

--- index.js
+++ index.js
@@ -69,8 +69,18 @@ async function run() {
 
     // middleware for verify token
     const verifyToken = (req, res, next) => {
-      console.log("middleware ", req.headers);
-      next();
+      // console.log("middleware ", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.envACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
     };
     app.get("/users", verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
