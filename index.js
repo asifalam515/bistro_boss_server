484962d4bedffdac19a56cee3945418a04dcc100
asifalam515 <asibulalam515@gmail.com>
2024-11-27 11:01:18 +0600

    middlware created

--- index.js
+++ index.js
@@ -45,28 +45,8 @@ async function run() {
       });
       res.send({ token });
     });
-    // users related API:
 
-    // make admin api route
-    app.patch("/users/admin/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          role: "admin",
-        },
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
-    // delete a specific user:
-    app.delete("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await userCollection.deleteOne(query);
-      res.send(result);
-    });
-
+    // custom middleware
     // middleware for verify token
     const verifyToken = (req, res, next) => {
       // console.log("middleware ", req.headers.authorization);
@@ -83,6 +63,44 @@ async function run() {
       });
     };
 
+    // verify whether admin or not:user verify admin after verify token
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+    // users related API:
+
+    // make admin api route
+    app.patch(
+      "/users/admin/:id",
+      verifyAdmin,
+      verifyToken,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+    );
+    // delete a specific user:
+    app.delete("/users/:id", verifyAdmin, verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       if (email !== req.decoded.email) {
@@ -96,7 +114,7 @@ async function run() {
       }
       res.send({ admin });
     });
-    app.get("/users", verifyToken, async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
